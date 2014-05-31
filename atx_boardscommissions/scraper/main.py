4166d5f0befc2ac9e5af21c1592990ce3d21719f
crccheck <c@crccheck.com>
2014-05-31 15:11:03 -0500

    begin process of grabbing real data

--- atx_boardscommissions/scraper/main.py
+++ atx_boardscommissions/scraper/main.py
@@ -1,8 +1,17 @@
+import requests
 from dateutil.parser import parse
 from lxml.html import document_fromstring
 
+
 MEETING_DATE = 'bcic_mtgdate'
 DOCUMENT = 'bcic_doc'
+PAGES = (
+    # slug, id
+    # slug: http://www.austintexas.gov/<slug>
+    # id: http://www.austintexas.gov/cityclerk/boards_commissions/meetings/<year>_<id>_<page>.htm
+    ('parb', '39'),
+    ('musiccomm', '12'),
+)
 
 
 def process_page(html):
@@ -44,5 +53,26 @@ def save_page(data, table):
         table.insert(row)
 
 
+def save_pages(table=None):
+    """
+    Save multiple pages.
+    """
+    for slug, pk in PAGES:
+        url = (
+            'http://www.austintexas.gov/cityclerk/boards_commissions/'
+            'meetings/{}_{}.htm'
+            .format(
+                pk,
+                '1',  # TODO assume only 1 page for now
+            )
+        )
+        response = requests.get(url)
+        assert response.status_code == 200
+        data = process_page(response.text)
+        if table:
+            save_page(data, table)
+        # TODO pause to avoid hammering
+
+
 if __name__ == '__main__':
-    process_page()
+    save_pages()
