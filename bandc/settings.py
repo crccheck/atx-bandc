1d8739a106c273ea283801a9dbd92e086940cb5d
crccheck <c@crccheck.com>
2014-06-01 15:50:00 -0500

    get more boards

--- bandc/settings.py
+++ bandc/settings.py
@@ -8,6 +8,8 @@ PAGES = (
     #     <year>_<id>_<page>.htm
     # name
     ('artcomm', '5', 'Arts Commission'),
+    ('hrc', '33', 'Human Rights Commission'),
     ('musiccomm', '12', 'Austin Music Commission'),
     ('parb', '39', 'Parks and Recreation Board'),
+    ('zpc', '54', 'Zoning and Platting Commission'),  # sic
 )
