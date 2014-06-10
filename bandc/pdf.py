e5007f142b69397a0cea413177d40374c684d8af
crccheck <c@crccheck.com>
2014-06-10 00:59:27 -0500

    a quick hack for turning my stored pdfs into images

--- bandc/pdf.py
+++ bandc/pdf.py
@@ -83,8 +83,48 @@ def grab_pdf(chunk=8):
             table.update(data, ['id'], ensure=False)
 
 
+def turn_pdfs_into_images():
+    """Run through the tmp directory and turn pdfs into images."""
+    from glob import glob
+    import sh
+    import boto
+    import boto.s3.connection
+    from boto.s3.connection import S3Connection
+    from boto.s3.key import Key
+    conn = S3Connection(
+        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+        host='objects.dreamhost.com',
+        calling_format=boto.s3.connection.OrdinaryCallingFormat(),
+    )
+    bucket = conn.get_bucket(os.environ.get('AWS_BUCKET'))
+    base_path = '/tmp/bandc_pdfs/'
+    for filepath in glob(os.path.join(base_path, '*.pdf')):
+        filepath.rsplit('.', 2)[0]
+        filename = os.path.basename(filepath)
+        pdf_id = os.path.splitext(filename)[0]
+        s3_key = '/thumbs/{}.jpg'.format(pdf_id)
+        print filepath, pdf_id, s3_key
+        k = bucket.get_key(s3_key)
+        if k:
+            print '.already exists'
+            continue
+        out = sh.convert(
+            filepath + '[0]',  # force to only get 1st page
+            '-thumbnail', '400x400',  # output size
+            '-alpha', 'remove',  # fix black border that appears
+            'jpg:-',  # force to output jpeg to stdout
+        )
+        k = Key(bucket)
+        k.key = s3_key
+        k.set_metadata('Content-Type', 'image/jpeg')
+        k.set_contents_from_string(out.stdout)
+        k.set_canned_acl('public-read')
+
+
 if __name__ == '__main__':
     count = 8
     if len(sys.argv) > 1:
         count = int(sys.argv[1])
-    grab_pdf(count)
+    # grab_pdf(count)
+    turn_pdfs_into_images()
