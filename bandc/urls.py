37878804e64501b9389cda919b68b5319bfb64e6
crccheck <c@crccheck.com>
2016-01-24 09:42:41 -0600

    only enable the admin in debug mode

--- bandc/urls.py
+++ bandc/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+if settings.DEBUG:
+    urlpatterns = [
+        url(r'^admin/', include(admin.site.urls)),
+    ]
+else:
+    urlpatterns = []
+
+urlpatterns += [
     url(r'', include('bandc.apps.agenda.urls', namespace='bandc')),
 ]
