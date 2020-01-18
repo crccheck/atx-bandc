1ae49cedb0c1ac90095bebf53721a0b12bb3172c
crccheck <c@crccheck.com>
2020-01-17 23:36:39 -0600

    update Django syntax

--- bandc/apps/agenda/apps.py
+++ bandc/apps/agenda/apps.py
@@ -1,7 +1,5 @@
-from __future__ import unicode_literals
-
 from django.apps import AppConfig
 
 
 class AgendaConfig(AppConfig):
-    name = 'agenda'
+    name = "agenda"
--- bandc/urls.py
+++ bandc/urls.py
@@ -1,19 +1,15 @@
 from django.conf import settings
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path, include
 from django.views.generic import TemplateView
 
+urlpatterns = [
+    path(
+        "robots.txt",
+        TemplateView.as_view(content_type="text/plain", template_name="robots.txt"),
+    ),
+    path("", include("bandc.apps.agenda.urls")),
+]
 
 if settings.DEBUG:
-    urlpatterns = [
-        url(r'^admin/', include(admin.site.urls)),
-    ]
-else:
-    urlpatterns = []
-
-urlpatterns += [
-    url(r'^robots.txt$', TemplateView.as_view(
-        content_type='text/plain', template_name='robots.txt')),
-
-    url(r'', include('bandc.apps.agenda.urls', namespace='bandc')),
-]
+    urlpatterns.append(path("admin/", admin.site.urls))
