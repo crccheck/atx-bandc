c80570db8a093e0f7dc8ae2a9f22039ea446503b
crccheck <c@crccheck.com>
2020-01-17 23:45:45 -0600

    update settings

--- bandc/settings.py
+++ bandc/settings.py
@@ -1,59 +1,59 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(__file__)
 
 import dj_database_url
+
 # import raven
 from project_runpy import env
 
-SECRET_KEY = env.get('SECRET_KEY', 'Rotom')
-DEBUG = env.get('DEBUG', False)
+SECRET_KEY = env.get("SECRET_KEY", "Rotom")
+DEBUG = env.get("DEBUG", False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'bandc.apps.agenda',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "bandc.apps.agenda",
     # support
-    'django_extensions',
-    'django_object_actions',
-    'bootstrap_pagination',
+    "django_extensions",
+    "django_object_actions",
+    "bootstrap_pagination",
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+MIDDLEWARE = (
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 )
 
-ROOT_URLCONF = 'bandc.urls'
+ROOT_URLCONF = "bandc.urls"
 
-WSGI_APPLICATION = 'bandc.wsgi.application'
+WSGI_APPLICATION = "bandc.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(default='sqlite:///bandc.db')}
+DATABASES = {"default": dj_database_url.config(default="sqlite:///bandc.db")}
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = "America/Chicago"
 
 USE_I18N = False
 
@@ -64,16 +64,17 @@ USE_TZ = True
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
-            'debug': DEBUG,
+            "debug": DEBUG,
         },
     },
 ]
@@ -82,52 +83,41 @@ TEMPLATES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 RAVEN_CONFIG = {
     # 'release': raven.fetch_git_sha(os.path.dirname(BASE_DIR)),
 }
-if 'RAVEN_DSN' in env:
-    RAVEN_CONFIG['dsn'] = env.get('RAVEN_DSN')
+if "RAVEN_DSN" in env:
+    RAVEN_CONFIG["dsn"] = env.get("RAVEN_DSN")
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'root': {
-        'level': os.environ.get('LOGGING_LEVEL', 'WARNING'),
-        'handlers': ['console'],
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {
+        "level": os.environ.get("LOGGING_LEVEL", "WARNING"),
+        "handlers": ["console"],
     },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-        'readable_sql': {
-            '()': 'project_runpy.ReadableSqlFilter',
-        },
+    "filters": {
+        "require_debug_false": {"()": "django.utils.log.RequireDebugFalse",},
+        "require_debug_true": {"()": "django.utils.log.RequireDebugTrue",},
+        "readable_sql": {"()": "project_runpy.ReadableSqlFilter",},
     },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'project_runpy.ColorizingStreamHandler',
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "project_runpy.ColorizingStreamHandler",
         },
     },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'DEBUG' if env.get('SQL', False) else 'INFO',
-            'handlers': ['console'],
-            'filters': ['require_debug_true', 'readable_sql'],
-            'propagate': False,
+    "loggers": {
+        "django.db.backends": {
+            "level": "DEBUG" if env.get("SQL", False) else "INFO",
+            "handlers": ["console"],
+            "filters": ["require_debug_true", "readable_sql"],
+            "propagate": False,
         },
-        'factory': {
-            'level': 'ERROR',
-            'propagate': False,
-        },
+        "factory": {"level": "ERROR", "propagate": False,},
     },
 }
