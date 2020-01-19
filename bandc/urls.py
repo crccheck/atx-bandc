e1837212542fee1fd8bce64a41e036d7b3947cfc
crccheck <c@crccheck.com>
2020-01-18 21:42:29 -0600

    serve media files

--- bandc/urls.py
+++ bandc/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -8,7 +9,7 @@ urlpatterns = [
         "robots.txt",
         TemplateView.as_view(content_type="text/plain", template_name="robots.txt"),
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns.append(path("admin/", admin.site.urls))
