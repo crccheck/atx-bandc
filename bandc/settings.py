4df50711e56b476450dcb7481fe1700f60933499
crccheck <c@crccheck.com>
2020-01-18 21:10:59 -0600

    add media settings

--- bandc/settings.py
+++ bandc/settings.py
@@ -83,6 +83,7 @@ TEMPLATES = [
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
+                "django.template.context_processors.media",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
@@ -99,6 +100,9 @@ TEMPLATES = [
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "media/"
 
 RAVEN_CONFIG = {
     # 'release': raven.fetch_git_sha(os.path.dirname(BASE_DIR)),
