a91ad0293d192a6034deb85d2e9f33e89cc1c610
Chris Chang <c@crccheck.com>
2020-01-20 17:39:00 -0600

    feat: add favicon so I can stop getting my tabs confused (#40)
    Add a simple favicon from an arbitrary Noun Project icon. My cropping was really rough but I don't have Photoshop anymore. This will help me identify which browser tabs are this project and differentiate production from development.

--- bandc/urls.py
+++ bandc/urls.py
@@ -2,9 +2,24 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+
+def favicon(request):
+    """
+    best by tezar tantular from the Noun Project
+    """
+    if settings.DEBUG:
+        image_data = open("static/favicon-dev.ico", "rb").read()
+    else:
+        image_data = open("static/favicon.ico", "rb").read()
+    # TODO add cache headers
+    return HttpResponse(image_data, content_type="image/x-icon")
+
+
 urlpatterns = [
+    path("favicon.ico", favicon),
     path(
         "robots.txt",
         TemplateView.as_view(content_type="text/plain", template_name="robots.txt"),
