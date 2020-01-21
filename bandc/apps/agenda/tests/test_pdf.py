feb2b26fd87b67b3891b97836d62a1d25b0e6199
Chris Chang <c@crccheck.com>
2020-01-21 11:50:34 -0600

    fix: Handle PDFTextExtractionNotAllowed pdfs (#42)
    These PDFs are extractable, but pdfminer won't extract them https://github.com/pdfminer/pdfminer.six/issues/350
    
    This forks the high level `extract_text` function to fix this. I could have combined `_get_pdf_page_count` with but then I wouldn't be able to delete this code in the future if pdfminer implements a fix.
    
    Part of #38

--- bandc/apps/agenda/tests/test_pdf.py
+++ bandc/apps/agenda/tests/test_pdf.py
@@ -1,7 +1,9 @@
 import os
 import unittest
+from unittest.mock import patch
 
-from ..pdf import _get_pdf_page_count
+from ..pdf import _get_pdf_page_count, process_pdf
+from ..factories import DocumentFactory
 
 
 BASE_DIR = os.path.dirname(__file__)
@@ -13,3 +15,22 @@ class PdfTest(unittest.TestCase):
             BASE_DIR, "samples/document_53B86715-0261-C36F-8C2F847EF15AD639.pdf"
         )
         self.assertEqual(_get_pdf_page_count(filepath), 5)
+
+    def test_get_pdf_page_count_handles_PDFTextExtractionNotAllowed(self):
+        filepath = os.path.join(BASE_DIR, "samples/edims_333704.pdf")
+        self.assertEqual(_get_pdf_page_count(filepath), 1)
+
+    @patch("bandc.apps.agenda.pdf._grab_pdf_thumbnail")
+    @patch("bandc.apps.agenda.pdf._download_document_pdf")
+    def test_extracts_extraction_not_allowed(self, mock_download, mock_thumbnail):
+        mock_download.return_value = os.path.join(BASE_DIR, "samples/edims_333704.pdf")
+        mock_thumbnail.return_value = b""
+        doc = DocumentFactory(
+            url="http://www.austintexas.gov/edims/document.cfm?id=333704"
+        )
+
+        process_pdf(doc)
+        doc.refresh_from_db()
+
+        self.assertEqual(doc.scrape_status, "scraped")
+        self.assertTrue(doc.text.startswith("Regular \nMeeting"))
