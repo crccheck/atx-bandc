18e3c9a3c0aef839b9b3c2b54f938d48fd64ed75
crccheck <c@crccheck.com>
2020-01-23 00:10:26 -0600

    reduce indent level

--- bandc/apps/agenda/management/commands/scrape_old.py
+++ bandc/apps/agenda/management/commands/scrape_old.py
@@ -8,14 +8,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         qs = Document.objects.filter(page_count=None).exclude(thumbnail="")
-        if qs.exists():
-            doc = qs[0]
-            self.stdout.write(f"Examining {doc.scrape_status} {doc}...")
-            doc.refresh()
-            self.stdout.write(
-                f'Re-scraped "{doc}" {doc.get_absolute_url()}, {qs.count()} left'
-            )
-        else:
+        if not qs.exists():
             self.stdout.write(
                 "No more missing Document.page_count we can delete the migration"
             )
+
+        doc = qs[0]
+        self.stdout.write(f"Examining {doc.scrape_status} {doc}...")
+        doc.refresh()
+        self.stdout.write(
+            f'Re-scraped "{doc}" {doc.get_absolute_url()}, {qs.count()} left'
+        )
