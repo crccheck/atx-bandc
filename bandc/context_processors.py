10bdb06deec660d244f4c9acc03f585e2d813742
crccheck <c@crccheck.com>
2020-02-01 01:23:02 -0600

    add a way to override because... reverse proxies

--- bandc/context_processors.py
+++ bandc/context_processors.py
@@ -1,2 +1,8 @@
+import os
+
+
 def base_url(request):
-    return {"BASE_URL": request.build_absolute_uri("/").rstrip("/")}
+    return {
+        "BASE_URL": os.getenv("BASE_URL")
+        or request.build_absolute_uri("/").rstrip("/"),
+    }
