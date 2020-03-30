d8e5f52d3598c7a74e56718a38806f95a5aaa39c
crccheck <c@crccheck.com>
2020-03-29 21:06:48 -0500

    add url

--- bandc/apps/agenda/urls.py
+++ bandc/apps/agenda/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, re_path
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 
 from .feeds import BandCDocumentFeed
-from .models import Document, Meeting
+from .models import Document, Meeting, ScrapeLog
 from .views import BandCList, BandCDetail, MeetingDetail
 
 
 urlpatterns = [
     path("", BandCList.as_view(), name="bandc_list"),
+    path("logs/", ListView.as_view(model=ScrapeLog), name="scrapelog_list"),
     path("<str:slug>/", BandCDetail.as_view(), name="bandc_detail"),
     path("feeds/<str:slug>/", BandCDocumentFeed(), name="feed"),
     path(
