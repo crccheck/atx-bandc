3220aec9d7c36314795c6c299615de3399606b73
crccheck <c@crccheck.com>
2025-03-02 04:15:39 +0000

    add wip task

--- bandc/apps/agenda/apps.py
+++ bandc/apps/agenda/apps.py
@@ -1,5 +1,22 @@
+import asyncio
+import logging
+
 from django.apps import AppConfig
+from django.core.management import call_command
+
+logger = logging.getLogger(__name__)
+
+
+async def periodic_scrape():
+    while True:
+        logger.info("hello")
+        # call_command("scrape")
+        await asyncio.sleep(60)
+        # await asyncio.sleep(60 * 60 * 24)
 
 
 class AgendaConfig(AppConfig):
     name = "bandc.apps.agenda"
+
+    def ready(self):
+        asyncio.create_task(periodic_scrape())
