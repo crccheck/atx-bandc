76635cb5ccd508bb0dca18957268e7ced54e508a
crccheck <c@crccheck.com>
2025-03-02 05:09:04 +0000

    install daphne

--- bandc/asgi.py
+++ bandc/asgi.py
@@ -7,10 +7,47 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
+import asyncio
+import logging
 import os
+import threading
 
+from asgiref.sync import sync_to_async
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bandc.settings")
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+logger = logging.getLogger(__name__)
+
+
+async def hello_task():
+    from django.core.management import call_command
+
+    @sync_to_async
+    def acall_scrape():
+        call_command("scrape")
+
+    while True:
+        await acall_scrape()
+        await asyncio.sleep(60 * 60)  # Sleep for 60 minutes
+
+
+# Track if the task is already running
+_task_started = False
+_startup_lock = threading.Lock()
+
+
+def ensure_background_task_started():
+    global _task_started
+
+    with _startup_lock:
+        if not _task_started:
+            asyncio.create_task(hello_task())
+            logger.info("Background task started")
+            _task_started = True
+
+
+async def application(*args, **kwargs):
+    ensure_background_task_started()
+    await django_application(*args, **kwargs)
