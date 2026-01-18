ba8c20835fc1b83c52472b579f00a3d9826c2359
crccheck <c@crccheck.com>
2026-01-18 07:39:16 +0000

    paginate by year instead

--- bandc/apps/agenda/views.py
+++ bandc/apps/agenda/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.utils.timezone import now
 from django.views.generic import DetailView, ListView
@@ -24,7 +25,6 @@ class BandCList(ListView):
 
 class BandCDetail(ListView):
     template_name = "agenda/bandc_detail.html"
-    paginate_by = 20
 
     def get_queryset(self, **kwargs):
         queryset = Document.objects.filter(active=True).select_related("meeting__bandc")
@@ -35,11 +35,42 @@ class BandCDetail(ListView):
             self.bandc = get_object_or_404(BandC, slug=self.kwargs["slug"])
             queryset = queryset.filter(meeting__bandc=self.bandc)
 
+        # Get available years from Meeting table (more efficient than through Document)
+        if self.bandc:
+            meetings_qs = Meeting.objects.filter(bandc=self.bandc)
+        else:
+            meetings_qs = Meeting.objects.all()
+
+        self.available_years = list(
+            meetings_qs.values_list("date__year", flat=True)
+            .distinct()
+            .order_by("-date__year")
+        )
+
+        # Parse year parameter and filter
+        year_param = self.request.GET.get("year")
+        if year_param:
+            try:
+                self.selected_year = int(year_param)
+            except ValueError:
+                raise Http404("Invalid year")
+            if self.selected_year not in self.available_years:
+                raise Http404("Year not found")
+        elif self.available_years:
+            self.selected_year = self.available_years[0]  # Default to most recent
+        else:
+            self.selected_year = None
+
+        if self.selected_year:
+            queryset = queryset.filter(meeting__date__year=self.selected_year)
+
         return queryset
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         data["bandc"] = self.bandc
+        data["available_years"] = self.available_years
+        data["selected_year"] = self.selected_year
         if self.bandc:
             data["upcoming_meetings"] = Meeting.objects.filter(
                 bandc=self.bandc,
